5dce689a42188081cb3afb17e3dcfe9814a40793
peshal77 <peshalnepal321@gmail.com>
2023-12-10 13:29:20 +0545

    slight error updated

--- src/components/FeaturedBooks.js
+++ src/components/FeaturedBooks.js
@@ -19,20 +19,7 @@ export const FeaturedBooks = () => {
           });
       }, []);
 
-      const handleViewMore = () => {
-        setState((prevState) => ({
-          ...prevState,
-          visibleLists: prevState.visibleLists + 4, // Increase by 4 or your desired value
-        }));
-      };
-    
-      const handleViewLess = () => {
-        setState((prevState) => ({
-          ...prevState,
-          visibleLists: prevState.visibleLists - 4, // Decrease by 4 or your desired value
-        }));
-      };
-
+     
       if(State.isLoading===true){
           return(
               <h1>Loading</h1>
