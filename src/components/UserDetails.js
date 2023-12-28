ee2ffde4928870a97019652586ff29011acb5c5f
kabishbrt <kabishbrt@gmail.com>
2023-12-28 22:26:33 +0545

    fixed bug

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const UserDetails = () => {
 
-    const [State, setState] = useState({ isLoading: true, book: [], status: 0 });
+    // const [State, setState] = useState({ isLoading: true, book: [], status: 0 });
   return (
     <h1>UserDetails</h1>
   )
