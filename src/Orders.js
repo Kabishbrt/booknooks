27f7c5f6687006d223e01c1988e1eb0cec7b42cb
kabishbrt <kabishbrt@gmail.com>
2024-01-01 21:38:33 +0545

    orders set up

--- src/Orders.js
+++ src/Orders.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useSelector,useDispatch } from 'react-redux';
 
 export const Orders = () => {
+  const {userid, loginalert} = useSelector((state) => state.auth);
+  //localhost:5000/orders/user/536558
+  //fetch this, destructure data and show it in a table
   return (
-    <h1>Orders</h1>
+    <h1>{userid}</h1>
   )
 }
