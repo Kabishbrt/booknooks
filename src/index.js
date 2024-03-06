02f1af238f91cc4f4ab1a123b6938ae5d8f9166f
kabishbrt <kabishbrt@gmail.com>
2024-03-06 20:06:55 +0545

    initial delay

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+setTimeout(() => {
 root.render(
   <React.StrictMode>
     <App />  
   </React.StrictMode>
 );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
